6fb5840b9faf76008b04259486c42dd6df836eb3
mikelcctstudent <2024044@student.cct.ie>
2026-05-18 12:46:48 +0100

    Update uber.py

--- uber.py
+++ uber.py
@@ -5,20 +5,22 @@ import numpy as np
 
 st.title("Uber Pickups in NYC")
 
-DATE_COLUMN="date/time"
+DATE_COLUMN = "date/time"
 
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-            
+
+
 def load_data(nrows):
-    data=pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase=lambda x: str(x).lower()
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis="columns", inplace=True)
-    data[DATE_COLUMN]=pd.to_datetime(data[DATE_COLUMN])
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
-    
-data_load_state=st.text("Loading Data...")
-data=load_data(10000)
+
+
+data_load_state = st.text("Loading Data...")
+data = load_data(10000)
 data_load_state.text("Loading Data...Done!")
 
 
@@ -26,5 +28,11 @@ st.subheader("Raw Data")
 st.write(data)
 
 st.subheader("Number of Pickups per hour")
-hist_values=np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24),[0])
-st.bar_chat(hist_values)
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour,
+    bins=24,
+    range=(0, 24)
+)[0]
+
+st.bar_chart(hist_values)
